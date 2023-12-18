3114d9c3a28ba6a2d900ac02d196e677ec70e07c
Billy Arnold <billyarnoldd@gmail.com>
2023-12-18 20:58:41 +0000

    Setting up passport jwt login function

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [AuthModule, UsersModule, ConfigModule.forRoot()],
+  controllers: [AppController, AuthController],
   providers: [AppService],
 })
 export class AppModule {}
